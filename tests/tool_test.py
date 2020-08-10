298faa50cccb470694e4e1f26a8f7f14ada5d724
pin-lpt <emiray58@gmail.com>
2020-08-10 14:44:34 +0800

    new repo

---
+++ tests/tool_test.py
@@ -0,0 +1,6 @@
+from distance import haversine
+
+def test_tool():
+	assert haversine(48.865070, 2.380009, 30, 30) != 1
+
+
---
+++ tool.py
@@ -0,0 +1,8 @@
+from distance import haversine
+
+def tool():
+	return haversine(48.865070, 2.380009, 30, 30)
+
+
+if __name__ == "__main__":
+    print(tool())
